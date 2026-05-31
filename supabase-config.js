fdfecd670d5759b40a3528b512ef99ab7b1a3284
david10102016 <dboni595@gmail.com>
2026-05-30 20:00:47 -0400

    first commit

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+// ================================================
+//  CONFIGURACIÓN SUPABASE — 100 Estudiantes Dicen
+//  Pegá tus datos aquí y guardá el archivo
+// ================================================
+
+const SUPABASE_URL = "https://tsqupqztmuipcjvrbhqj.supabase.co"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzcXVwcXp0bXVpcGNqdnJiaHFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxNzYyOTIsImV4cCI6MjA5NTc1MjI5Mn0.OvfqzxkhOp0sWlK_K-fnvnoXV-CA-rofB01dPR3hi2Q"
